d2cdcc905ea9f3a4b0a62e3c1481120d38017d42
vunguyen <vu23697@gmail.com>
2020-04-05 21:24:04 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './frontend/views/react/redux/App';
+import './frontend/views/scss/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);

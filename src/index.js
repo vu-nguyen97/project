562a767e02e660f3b0289788346b5e6eaa17aa72
vunguyen <vu23697@gmail.com>
2020-04-06 22:37:29 +0700

    Config .eslintrc.json file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './frontend/views/react/redux/App';
 import './frontend/views/scss/index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <App />,

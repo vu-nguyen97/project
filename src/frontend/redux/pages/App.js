0fbc4135a9f0c997098e108b78c8794d4a07b99f
vunguyen <vu23697@gmail.com>
2020-04-07 07:54:06 +0700

    Edit file structure

--- src/frontend/redux/pages/App.js
+++ src/frontend/redux/pages/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import ReactRouter from '../../routes/index.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        heellooo 
-        <Button>
-          Signin
-        </Button>
+        <ReactRouter />
       </div>
     );
   }
